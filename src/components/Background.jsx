cf36429dbec73aa79e419208de7412117f1948f2
NicolasM-83200 <manigand.nicolas@gmail.com>
2024-01-29 13:41:45 +0100

    update component background pour affichage dynamique entre video et image

---
+++ src/components/Background.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import videoBackground2 from '../assets/video-background-2.mp4';
+import background from '../assets/logo-ngu.png';
+import { useLocation } from 'react-router-dom';
+
+const Background = () => {
+  const location = useLocation();
+  return (
+    <div className='background'>
+      {location.pathname === '/' ? (
+        <video width={1660} height={841} autoPlay muted loop>
+          <source src={videoBackground2} type='video/mp4' />
+        </video>
+      ) : (
+        <img src={background} alt='background ngu' width={1230} height={1280} />
+      )}
+    </div>
+  );
+};
+
+export default Background;
