0ab437855786624eb02733868313607b63f542e7
NicolasM-83200 <manigand.nicolas@gmail.com>
2024-02-01 11:45:04 +0100

    update page Videos

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,24 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import videos from '../data/videos.json';
 import { motion } from 'framer-motion';
 
 const Videos = () => {
-  const [videos, setVideos] = useState([]);
-
-  const channelId = 'UCIbuERcgR2AAIj74j7twMmw';
-  const url = `https://www.googleapis.com/youtube/v3/search?key=${
-    import.meta.env.VITE_API_KEY
-  }&channelId=${channelId}&part=snippet,id&order=date&maxResults=10`;
-  console.log(url);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(url);
-      const result = await response.json();
-      setVideos(result);
-    };
-    fetchData();
-  }, []);
-
   return (
     <motion.section
       id='videos'
@@ -27,18 +11,21 @@ const Videos = () => {
       exit={{ opacity: 0, transition: { duration: 0.3 } }}
     >
       <div className='videos-container'>
-        {videos.items?.map((video) => (
-          <iframe
-            key={video.etag}
-            width='560'
-            height='315'
-            src={`https://www.youtube.com/embed/${video.id.videoId}`}
-            title={video.snippet.title}
-            frameBorder='0'
-            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-            allowFullScreen
-          ></iframe>
-        ))}
+        {videos
+          .slice()
+          .reverse()
+          .map((video) => (
+            <iframe
+              key={video.id}
+              width='560'
+              height='315'
+              src={video.url}
+              title={video.title}
+              frameBorder='0'
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+              allowFullScreen
+            ></iframe>
+          ))}
       </div>
     </motion.section>
   );
