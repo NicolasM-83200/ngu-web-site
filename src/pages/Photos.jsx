508b3123ff9651997b618d4f6d86ff61bfafb6da
NicolasM-83200 <manigand.nicolas@gmail.com>
2024-02-01 11:44:14 +0100

    update page Photos

--- src/pages/Photos.jsx
+++ src/pages/Photos.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
+import { getAlbums } from '../lib/common';
 
 const Photos = () => {
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    async function getAlbumsList() {
+      const data = await getAlbums();
+      setAlbums(data);
+    }
+    getAlbumsList();
+  }, []);
+
   return (
     <motion.section
       id='photos'
@@ -9,9 +21,23 @@ const Photos = () => {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0, transition: { duration: 0.3 } }}
     >
-      <div className='photos-container'>
-        <h1>Photos Page</h1>
-        <h2>Galerie photos</h2>
+      <h2>Photos Page</h2>
+      <div className='album-container'>
+        {albums.map((album) => (
+          <Link
+            className='album-container__link'
+            to={`/photos/${album.id}`}
+            key={album.id}
+            state={album}
+          >
+            <img
+              src={album.picture.data.url}
+              alt={album.name}
+              className='album-container__image'
+              width={180}
+            />
+          </Link>
+        ))}
       </div>
     </motion.section>
   );
