939825f3ca4b260326521463691b65e9e7b24c57
NicolasM-83200 <manigand.nicolas@gmail.com>
2024-02-01 14:47:29 +0100

    ajout composant footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <p>&#169; Never Give Up Toulon</p>
+      <span>
+        Alex GAYON | Athmane BOUZID | Nicolas MANIGAND | Yann RUAUX | Gianni
+        CHARLIER
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
