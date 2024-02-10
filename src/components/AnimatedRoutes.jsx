0f163227a2c2f49a82cee1bbfddb7a9844e43d2b
NicolasM-83200 <manigand.nicolas@gmail.com>
2024-02-10 15:32:52 +0100

    update composant AnimatedRoutes pour ajout de la route 'photosOf'

--- src/components/AnimatedRoutes.jsx
+++ src/components/AnimatedRoutes.jsx
@@ -9,6 +9,7 @@ import Merch from '../pages/Merch';
 import Contact from '../pages/Contact';
 import PhotosAlbum from '../pages/PhotosAlbum';
 import PhotosBy from '../pages/PhotosBy';
+import PhotosOf from '../pages/PhotosOf';
 
 const AnimatedRoutes = () => {
   const location = useLocation();
@@ -19,6 +20,7 @@ const AnimatedRoutes = () => {
         <Route path='/photos' element={<Photos />} />
         <Route path='/photos/:albumId' element={<PhotosAlbum />} />
         <Route path='/photos/photos_by' element={<PhotosBy />} />
+        <Route path='/photos/photos_of' element={<PhotosOf />} />
         <Route path='/videos' element={<Videos />} />
         <Route path='/tour' element={<Tour />} />
         <Route path='/merch' element={<Merch />} />
