391d54c453dd856926010cfc4732510668017bde
NicolasM-83200 <manigand.nicolas@gmail.com>
2024-02-10 15:34:01 +0100

    update url graph Api facebook pour récupérer photos du journal

--- src/lib/common.js
+++ src/lib/common.js
@@ -6,7 +6,7 @@ export async function getDatas() {
       // `https://graph.facebook.com/v19.0/me/albums?fields=photos%7Bid%2Cimages%2Cname%2Cheight%2Cwidth%7D%2Cpicture%2Cname&access_token=${
       //   import.meta.env.VITE_ACCESS_TOKEN
       // }`
-      `https://graph.facebook.com/v19.0/me?fields=id%2Calbums%7Bpicture%7Burl%7D%2Cname%2Cphotos.limit(25)%7Bheight%2Cwidth%2Cwebp_images%2Cname%7D%7D%2Cphotos.limit(25)%7Bheight%2Cwebp_images%2Cwidth%2Cname%7D%2Cvideos%7Bsource%2Cthumbnails%2Ctitle%7D&access_token=${
+      `https://graph.facebook.com/v19.0/me?fields=photos%7Balbum%2Cwebp_images%7D%2Calbums%7Bname%2Cpicture%7Burl%7D%2Cphotos%7Balbum%2Cwebp_images%7D%7D%2Cfeed%7Bfull_picture%7D&access_token=${
         import.meta.env.VITE_ACCESS_TOKEN
       }`
     );
