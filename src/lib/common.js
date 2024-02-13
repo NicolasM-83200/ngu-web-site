d1577fee697911e85aea486ea97f4839156ada67
NicolasM-83200 <manigand.nicolas@gmail.com>
2024-02-13 12:02:23 +0100

    ajout highlight sur social media

--- src/lib/common.js
+++ src/lib/common.js
@@ -16,3 +16,33 @@ export async function getDatas() {
     return null;
   }
 }
+
+export const mouseMoveHandler = (element, e) => {
+  let elRect = element.getBoundingClientRect();
+
+  let x = e.clientX - elRect.x;
+  let y = e.clientY - elRect.y;
+
+  let midCardWidth = elRect.width / 2;
+  let midCardHeight = elRect.height / 2;
+
+  let angleY = -(x - midCardWidth) / 4;
+  let angleX = (y - midCardHeight) / 4;
+
+  let highlightX = (x / elRect.width) * 100;
+  let highlightY = (y / elRect.height) * 100;
+
+  element.children[0].style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg) scale(1.1)`;
+  element.children[1].style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg) scale(1.1)`;
+  element.children[1].style.background = `radial-gradient(
+    circle at ${highlightX}% ${highlightY}%,
+    rgb(184, 196, 211),
+    transparent)`;
+};
+
+export const mouseLeaveHandler = (element) => {
+  element.addEventListener('mouseleave', () => {
+    element.children[0].style.transform = 'rotateX(0) rotateY(0)';
+    element.children[1].style.transform = 'rotateX(0) rotateY(0)';
+  });
+};
